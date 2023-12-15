79d8906e44b03b5efb4041e661896955cc4ad0d3
Auridebson Lima <auridebson@vutus.com>
2023-12-15 10:35:38 -0300

    Update

--- main151223.py
+++ main151223.py
@@ -4,18 +4,17 @@ def ln(x):
 frutas = ["Maça","Banana","Goiaba","Pera","Limão"]
 familia = ["Auridebson","Luciana","Guilherme","Levy"]
 numeros = [22,34,66,92,74,19,90,98,32,35,61]
+palavra = ["p","y","t","h","o","n"]
 
 def pegaLista(lis):
-    res = ""
-    for item in lis:
-        if enumerate(item) == True:
-            res += item
-        else:
-            res += f'{item}\n'
-    return res
+    maior_nota = lis[0]
+    for nota_atual in lis:
+        if  nota_atual > maior_nota:
+            maior_nota = nota_atual
+    return maior_nota
 
 ln(30)
 print(pegaLista(numeros))
-# for fruta in frutas:
-#     print(fruta)
 ln(30)
+
+
