5fddc11b0a4cd65dbc694888448b535de0ed774f
Auridebson Lima <auridebson@vutus.com>
2023-12-15 12:25:05 -0300

    Update

--- main_lambda.py
+++ main_lambda.py
@@ -8,7 +8,13 @@
 
 # menu = int(input("Escolha uma opção para fazer os cálculos:\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[5] - Potencialização\n"))
 
-entrada = int(input("Digite um número inteiro: "))
-numero = lambda entrada : "Número par" if (entrada % 2 == 0) else "Número ímpar"
+# entrada = int(input("Digite um número inteiro: "))
+# numero = lambda entrada : "Número par" if (entrada % 2 == 0) else "Número ímpar"
 
-print(numero)
+# print(numero(entrada))
+
+semaforo = input("Digite a cor do semaforo: ")
+
+acao = lambda agir : "Parar" if semaforo == "vermelho" else "Siga" if semaforo == "verde" else "Acelere" if semaforo == "amarelo"
+
+print(acao(semaforo))
---
+++ main_map.py
@@ -0,0 +1,4 @@
+numeros = [22,51,56,22,34,77,54,82,19,85]
+pares = []
+
+pares = list(map(lambda numero : numero if numero%2==0))
